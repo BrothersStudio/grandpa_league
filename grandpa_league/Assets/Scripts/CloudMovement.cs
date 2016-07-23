b8303673db024915cc7fdb2228774403859c1f98
Kevin <Kevin>
2016-07-23 13:56:16 -0400

    Clouds are now in the background, moving and looping

--- grandpa_league/Assets/Scripts/CloudMovement.cs
+++ grandpa_league/Assets/Scripts/CloudMovement.cs
@@ -4,17 +4,25 @@ using System.Collections;
 public class CloudMovement : MonoBehaviour
 {
     public Vector2 velocity = Vector2.zero;
+    public Vector3 startPos;
 
     private Rigidbody2D body2d;
 
     void Awake()
     {
+        startPos = transform.position;
         body2d = GetComponent<Rigidbody2D>();
     }
 
     void FixedUpdate()
     {
         body2d.velocity = velocity;
+        print(startPos);
+        print(transform.position);
+        if(transform.position.x >= (startPos.x + 120))
+        {
+            transform.position = startPos;
+        }
     }
 
 }
