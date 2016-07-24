9c90cbe08798b837b89a449c2935a7882b7bd35c
Kevin <Kevin>
2016-07-23 20:24:48 -0400

    Added sound effects when clicking on buttons

---
+++ grandpa_league/Assets/Scripts/SoundEffectPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class SoundEffectPlayer : MonoBehaviour {
+
+    public AudioClip[] clicks;
+    public AudioSource clickSource;
+
+
+    //choses a random click noise to play then plays it
+    public void PlayClick()
+    {
+        int randClip = Random.Range(0, clicks.Length);
+        clickSource.clip = clicks[randClip];
+        clickSource.Play();
+    }
+}
