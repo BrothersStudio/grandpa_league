0ea0f181af6ebec789b77d2b3d734c6be6f053bc
crc5464 <crc5464@psu.edu>
2016-07-24 22:15:50 -0400

    Adding debugging to event handler`

--- grandpa_league/Assets/Scripts/SimulationEvent.cs
+++ grandpa_league/Assets/Scripts/SimulationEvent.cs
@@ -55,6 +55,7 @@ public class SimulationEvent
         }
         catch (Exception e)
         {
+            UnityEngine.Debug.Log(string.Format("{0}, {1}, {2}", e.Source, e.Message, e.StackTrace));
             this.m_outcome = new Outcome();
             this.m_outcome.Mail = new Mail();
             this.m_outcome.Mail.Message = string.Format("Oops,\n\n Sorry about that little hiccup. Your event crashed (we weren't expecting that to happen). If you see any developers around tell them this:\n\n{0}\n{1}\n{2}", e.Source, e.Message, e.StackTrace.Substring(0, 200));
