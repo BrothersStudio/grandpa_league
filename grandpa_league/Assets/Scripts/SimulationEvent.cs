226c11f86d93f6e64d920678e67db4c93b5c3054
crc5464 <crc5464@psu.edu>
2016-07-25 23:32:56 -0400

    Adding stat class and changing how stats are done on the backend

--- grandpa_league/Assets/Scripts/SimulationEvent.cs
+++ grandpa_league/Assets/Scripts/SimulationEvent.cs
@@ -58,7 +58,9 @@ public class SimulationEvent
             UnityEngine.Debug.Log(string.Format("{0}, {1}, {2}", e.Source, e.Message, e.StackTrace));
             this.m_outcome = new Outcome();
             this.m_outcome.Mail = new Mail();
-            this.m_outcome.Mail.Message = string.Format("Oops,\n\n Sorry about that little hiccup. Your event crashed (we weren't expecting that to happen). If you see any developers around tell them this:\n\n{0}\n{1}\n{2}", e.Source, e.Message, e.StackTrace.Substring(0, 200));
+            this.m_outcome.Mail.Subject = "Re: oops!";
+            this.m_outcome.Mail.Sender = "Three Brothers Studio Chris";
+            this.m_outcome.Mail.Message = string.Format("Oops,\n\n Sorry about that little hiccup. Your event crashed (we weren't expecting that to happen). If you see any developers around tell them this:\n\n{0}\n{1}\n{2}", e.Source, e.Message, e.StackTrace.Substring(0, 350));
         }
         return this.m_outcome;
     }
