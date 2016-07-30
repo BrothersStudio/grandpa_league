b52a1460124ff67632441a239e331d01004fa6e5
Kevin <Kevin>
2016-07-30 01:16:11 -0400

    Cursor should be working now for real

--- grandpa_league/Assets/Scripts/BirdFlying.cs
+++ grandpa_league/Assets/Scripts/BirdFlying.cs
@@ -5,6 +5,8 @@ public class BirdFlying : MonoBehaviour {
 
     public Vector2 velocity = Vector2.zero;
     public Vector3 StartPos;
+    public AudioSource chirpSource;
+    public AudioClip chirp;
 
     private Rigidbody2D body2d;
 
@@ -12,11 +14,19 @@ public class BirdFlying : MonoBehaviour {
     {
         body2d = GetComponent<Rigidbody2D>();
         StartPos = transform.position;
+        chirpSource.clip = chirp;
 	}
 	
-	// Update is called once per frame
 	void FixedUpdate ()
     {
         body2d.velocity = velocity;
 	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Sky"))
+        {
+            chirpSource.Play();
+        }
+    }
 }
