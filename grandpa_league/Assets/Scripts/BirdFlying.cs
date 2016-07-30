8aecba02912513aa07f09b00101254f7a28538e0
Kevin <Kevin>
2016-07-29 22:44:28 -0400

    Added birds flying to starting menu and added missing sound effects

---
+++ grandpa_league/Assets/Scripts/BirdFlying.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdFlying : MonoBehaviour {
+
+    public Vector2 velocity = Vector2.zero;
+    public Vector3 StartPos;
+
+    private Rigidbody2D body2d;
+
+	void Awake ()
+    {
+        body2d = GetComponent<Rigidbody2D>();
+        StartPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        body2d.velocity = velocity;
+	}
+}
