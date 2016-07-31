d4cea4128192921b704e6c3eeded0a9c1e2a4a2b
crc5464 <crc5464@psu.edu>
2016-07-31 13:11:24 -0400

    Adding default first name and also input checking

--- grandpa_league/Assets/Scripts/Constants.cs
+++ grandpa_league/Assets/Scripts/Constants.cs
@@ -82,6 +82,7 @@ static class Constants
         public const double INITIAL_MONEY = 500;
         public const double INITIAL_INCOME = 1000;
         public const string DEFAULT_SURNAME = "Smith";
+        public const string DEFAULT_FIRST_NAME = "John";
         public const double INITIAL_WISDOM = 50;
         public const double INITIAL_WISDOM_GROWTH = -0.04;
         public const double INITIAL_INSANITY = 20;
