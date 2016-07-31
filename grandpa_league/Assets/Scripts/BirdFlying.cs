c3136ec54a07c0d1a2b8cc3c66550ddeb9f56322
Kevin <Kevin>
2016-07-31 19:23:21 -0400

    Added birds flying on main scene and they now repeat

--- grandpa_league/Assets/Scripts/BirdFlying.cs
+++ grandpa_league/Assets/Scripts/BirdFlying.cs
@@ -4,18 +4,34 @@ using System.Collections;
 public class BirdFlying : MonoBehaviour {
 
     public Vector2 velocity = Vector2.zero;
-    public Vector3 StartPos;
-
+    public Vector3 startPos;
+    public int waitTime;
     private Rigidbody2D body2d;
 
 	void Awake ()
     {
         body2d = GetComponent<Rigidbody2D>();
-        StartPos = transform.position;
+        startPos = transform.position;
+        
 	}
+
+    void Start ()
+    {
+        StartCoroutine(RestartPosition());
+    }
 	
 	void FixedUpdate ()
     {
         body2d.velocity = velocity;
 	}
+
+    IEnumerator RestartPosition()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(waitTime);
+            transform.position = startPos;
+        }
+        
+    }
 }
